c99e818cf4da376e44ef2e38083be009bf1538be
JesseLivezey <jesse.livezey@gmail.com>
2016-09-02 10:52:11 -0700

    added files

---
+++ utils.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+
+def preprocess(df, t_range):
+    stations = sorted(set(df['STATION_NAME']))
+    df['DATE_fmt'] = pd.to_datetime(df['DATE'], format='%Y%m%d')
+    data_per_station = dict()
+    for t in ['TMAX', 'TMIN', 'TAVG']:
+        df[t] = df[t].apply(lambda x: np.nan if x < t_range[0] else x)
+        df[t] = df[t].apply(lambda x: np.nan if x > t_range[1] else x)
+    for st in stations:
+        data_per_station[st] = df[df['STATION_NAME'] == st]
+    return df, stations, data_per_station
