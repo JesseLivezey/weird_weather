3b0a8fa78ac557d634faf8949f548263575ce971
JesseLivezey <jesse.livezey@gmail.com>
2016-09-13 12:31:25 -0700

    small index change

--- utils.py
+++ utils.py
@@ -5,6 +5,7 @@ import pandas as pd
 def preprocess(df, t_range):
     stations = sorted(set(df['STATION_NAME']))
     df['DATE_fmt'] = pd.to_datetime(df['DATE'], format='%Y%m%d')
+    df.set_index(['DATE_fmt'])
     data_per_station = dict()
     for t in ['TMAX', 'TMIN', 'TAVG']:
         df[t] = df[t].apply(lambda x: np.nan if x < t_range[0] else x)
